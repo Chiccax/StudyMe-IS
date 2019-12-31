3f0678ab54c2d3dfca51fe657fb84de99a797019
mariarachele <51359905+mariarachele@users.noreply.github.com>
2019-12-31 13:53:21 +0100

    Implementazione RecensioneServlet e TerminaOrdineServlet

--- Source/src/control/RecensioneServlet.java
+++ Source/src/control/RecensioneServlet.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,32 +9,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.PrintWriter;
-
-
 import com.google.gson.Gson;
 
 import control.util.JSONResponse;
 import modelDao.RecensioneDao;
 
-
 @WebServlet("/RecensioneServlet")
 public class RecensioneServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+   
     public RecensioneServlet() {
         super();
-       
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		Gson gson = new Gson();
 		
-		doGet(request, response);
+		String nomeUtente = request.getParameter("utente");
+		String codicePacchetto = request.getParameter("codicePacchetto");
+		String titoloRecensione = request.getParameter("titoloRecensione");
+		String testoRecensione = request.getParameter("testoRecensione");
+		
+		if(nomeUtente == null || codicePacchetto == null || titoloRecensione == null || testoRecensione == null) {
+			JSONResponse jsonResponse = new JSONResponse(false, NO_ARGUMENT);
+			out.print(gson.toJson(jsonResponse));
+			return;
+		}
+		
+		RecensioneDao recensionedao = new RecensioneDao();	
+		recensionedao.aggiungiRecensione(nomeUtente, codicePacchetto, titoloRecensione, testoRecensione);
+		JSONResponse jsonResponse = new JSONResponse(true);
+		out.print(gson.toJson(jsonResponse));
 	}
-
+	
+	private static final String NO_ARGUMENT = "Tutti i parametri devono essere passati";
 }
